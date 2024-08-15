516ea81969e04e16385a23411e89a1c3881582f8
goongie <wannarat.p@gmail.com>
2024-08-15 23:28:49 +0700

    Complete initial setup of Hotwire

---
+++ piazza-ios/Piazza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piazza
+//
+//  Created by Wannarat Chitcharus on 15/8/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
